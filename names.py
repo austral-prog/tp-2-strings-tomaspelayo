b486b2fc67b301961061e0aeff0b5e0f615ab45e
tomaspelayo <tfernandezpelayo@mail.austral.edu.ar>
2026-03-17 10:35:23 -0300

    Update names.py

--- names.py
+++ names.py
@@ -2,4 +2,12 @@ def names():
     """Lee nombre y apellido, e imprime el nombre completo en distintos formatos:
     minúsculas, título, mayúsculas y con tabulador.
     """
-    pass
+    
+    
+    nombre = input("Ingrese el nombre ")
+    apellido = input("Ingrese el apellido ")
+    nombre_completo = (nombre + " " + apellido )
+    print(nombre_completo.lower())
+    print(nombre_completo.title())
+    print(nombre_completo.upper())
+    print("\t" + nombre_completo.lower())
