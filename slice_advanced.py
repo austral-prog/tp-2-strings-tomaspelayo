d7839b8708de48249d0ef5803a0bb98680c86feb
tomaspelayo <tfernandezpelayo@mail.austral.edu.ar>
2026-03-17 12:55:47 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -2,4 +2,5 @@ def slice_advanced():
     """Lee un texto e imprime los caracteres desde la posición 4
     en adelante, tomando uno de cada dos (paso 2).
     """
-    pass
+    texto = input("Ingrese un texto: ")
+    print(texto[4::2])
