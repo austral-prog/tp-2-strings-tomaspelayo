c50a94450a94b1af8f17ee5868f5bd0c00defa47
tomaspelayo <tfernandezpelayo@mail.austral.edu.ar>
2026-03-27 09:38:49 -0300

    Update string_methods.py

--- string_methods.py
+++ string_methods.py
@@ -6,5 +6,23 @@ def string_methods():
     nombre = "   Grace Hopper   "
     frase = "Python es un gran lenguaje de programacion"
     multilinea = """Linea 1
-    Linea 2
-    Linea 3"""
+Linea 2
+Linea 3"""
+
+    palabra = "Python"
+    print(f"Strip: {nombre.strip()}")
+    print(f"Lstrip: {nombre.lstrip()}")
+    print(f"Rstrip: {nombre.rstrip()}")
+    print(f"Upper: {frase.upper()}")
+    print(f"Lower: {frase.lower()}")
+    print(f"Title: {frase.title()}")
+    print(f"Find: {frase.find('gran')}")
+    print(f"Replace: {frase.replace('programacion', 'desarrollo')}")
+    print(f"Count: {frase.count('a')}")
+    print(f"Contiene Python: {'Python' in frase}")
+    print(f"Contiene Java: {'Java' in frase}")
+    print(f"Slice: {frase[0:6]}")
+    print(f"Paso: {palabra[::2]}")
+    print(f"Reverso: {palabra[::-1]}")
+    print(f"Formato: {nombre.strip() + ' ' +'sabe' + ' ' +palabra}")
+    print(multilinea)
